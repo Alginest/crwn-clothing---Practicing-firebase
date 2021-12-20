f9f0ebd76cebfd3f37d76a558df446846a527663
ado durak <76266634+Alginest@users.noreply.github.com>
2021-12-20 15:31:49 +0100

    Add files via upload

---
+++ src/pages/singIn-and-signUp/SignInandSignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/signIn/signIn";
+const SignInandSignUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInandSignUp;
